610c9f2df968441ed80f44c45970b4124f2e9fa8
Drew Bloomfield <DrewBloomfield@Drews-MacBook-Air.local>
2018-08-28 09:31:04 -0500

    add in icon images

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow } = require('electron')
+var path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -7,7 +8,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 250, height: 250})
+  mainWindow = new BrowserWindow({
+    width: 250,
+    height: 250,
+    icon: path.join(__dirname, 'icons/png/64x64.png')
+  })
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
